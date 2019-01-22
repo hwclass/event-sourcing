d00fb9ac5e6361e76222beb7d92ac5bc2e881a61
Baris Güler <bgueler@M20160009.fritz.box>
2019-01-22 21:38:41 +0100

    Fix for moment format

--- Timestamp.ts
+++ Timestamp.ts
@@ -1,39 +1,41 @@
-import * as moment from 'moment'
+import moment from "moment";
 
 class Timestamp {
-    private timestamp: string;
-    private static format = 'YYYY-MM-DD HH:mm:ss'
+  private timestamp: string;
+  private static format = "YYYY-MM-DD HH:mm:ss";
 
-    private constructor(timestamp: string) {
-        if (timestamp === '' && !Timestamp.isValid(timestamp)) {
-            throw Error('Invalid timestamp')
-        }
-        this.timestamp = moment(timestamp).format(Timestamp.format)
+  private constructor(timestamp: string) {
+    if (timestamp === "" && !Timestamp.isValid(timestamp)) {
+      throw Error("Invalid timestamp");
     }
+    this.timestamp = moment("2013-03-01", "YYYY-MM-DD HH:mm:ss").format(
+      Timestamp.format
+    );
+  }
 
-    private static isValid(timestamp: string): Boolean {
-        return moment(timestamp, this.format, true).isValid()
-    }
+  private static isValid(timestamp: string): Boolean {
+    return moment(timestamp, this.format, true).isValid();
+  }
 
-    public toString(): string {
-        return moment(this.timestamp).format(Timestamp.format)
-    }
+  public toString(): string {
+    return moment(this.timestamp).format(Timestamp.format);
+  }
 
-    public static fromString(timestamp: string): Timestamp {
-        return new Timestamp(timestamp)
-    }
+  public static fromString(timestamp: string): Timestamp {
+    return new Timestamp(timestamp);
+  }
 
-    public equals(that: Timestamp): Boolean {
-        return that.timestamp === this.timestamp
-    }
+  public equals(that: Timestamp): Boolean {
+    return that.timestamp === this.timestamp;
+  }
 
-    public isBefore(that: Timestamp): Boolean {
-        return moment(this.timestamp).isBefore(that.timestamp)
-    }
+  public isBefore(that: Timestamp): Boolean {
+    return moment(this.timestamp).isBefore(that.timestamp);
+  }
 
-    public isAfter(that: Timestamp): Boolean {
-        return moment(this.timestamp).isAfter(that.timestamp)
-    }
+  public isAfter(that: Timestamp): Boolean {
+    return moment(this.timestamp).isAfter(that.timestamp);
+  }
 }
 
-export default Timestamp
+export default Timestamp;
