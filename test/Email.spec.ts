ff1ba2b1c5484e1ff89039a1a1f54659380b7f89
Baris Guler <harderworking.class@mgmail.com>
2019-01-23 10:19:16 +0100

    Add Email test file

---
+++ test/Email.spec.ts
@@ -0,0 +1,30 @@
+import Timestamp from "../src/Timestamp";
+import { expect } from "chai";
+import "mocha";
+import Email from "../src/Email";
+
+describe("Email", () => {
+  const invalidEmail = "some@";
+  const validEmail = "some@example.com";
+
+  describe("constructor", () => {
+    it("should throw an error for invalid email", () => {
+      expect(() => new Email(invalidEmail)).to.throw(
+        "Please enter a valid email"
+      );
+    });
+
+    it("should not throw an error for valid email", () => {
+      expect(() => new Email(validEmail)).not.to.throw(
+        "Please enter a valid email"
+      );
+    });
+  });
+
+  describe("toString()", () => {
+    it("should return valid email in string", () => {
+      const email = new Email(validEmail);
+      expect(email.toString()).to.equal(validEmail);
+    });
+  });
+});
